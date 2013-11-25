acc9ff0877870f9a1fdd3aa941d4dc94ecb7a2f9
SirLolloDoD@gmail.com <SirLolloDoD@gmail.com@1bb21479-9aac-b773-212e-4918d035b54c>
2013-11-25 16:00:17 +0000

    Aggiunta Activity Profile, con cattura foto e ridimensionamento, salvataggio in cartella dell'applicazione. Rivisti campi necessari all'immagine del profilo nel DB.

---
+++ UMessage/src/com/lollotek/umessage/activities/Profile.java
@@ -0,0 +1,284 @@
+package com.lollotek.umessage.activities;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.lollotek.umessage.R;
+import com.lollotek.umessage.UMessageApplication;
+import com.lollotek.umessage.utils.Utility;
+
+public class Profile extends Activity {
+
+	private ImageView iv;
+	private Button b;
+	private Context context;
+	private final int PICK_IMAGE = 0;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		context = this;
+
+		setContentView(R.layout.activity_profile);
+
+		iv = (ImageView) findViewById(R.id.imageView1);
+		b = (Button) findViewById(R.id.button1);
+		ActionBar ab = getActionBar();
+		ab.setDisplayHomeAsUpEnabled(true);
+
+		b.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				File mainFolder = Utility.getMainFolder(context);
+
+				File myNewProfileImage = new File(mainFolder.toString()
+						+ "/me.jpg");
+
+				Intent i = new Intent(
+						android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+				i.putExtra(MediaStore.EXTRA_OUTPUT,
+						Uri.fromFile(myNewProfileImage));
+				startActivityForResult(i, PICK_IMAGE);
+			}
+		});
+
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		loadProfileImage();
+	}
+
+	public void loadProfileImage() {
+		File mainFolder = Utility.getMainFolder(UMessageApplication
+				.getContext());
+		File myProfileImage = new File(mainFolder.toString() + "/me.jpg");
+		if (myProfileImage.exists()) {
+			iv.setImageURI(Uri.fromFile(myProfileImage));
+		}
+
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		try {
+			File mainFolder = Utility.getMainFolder(context);
+
+			File myNewProfileImage = new File(mainFolder.toString() + "/me.jpg");
+
+			compressImage(myNewProfileImage.toString());
+			
+			loadProfileImage();
+			
+		} catch (Exception e) {
+			
+		}
+
+	}
+
+	public String compressImage(String imageUri) {
+
+		String filePath = getRealPathFromURI(imageUri);
+		Bitmap scaledBitmap = null;
+
+		BitmapFactory.Options options = new BitmapFactory.Options();
+
+		// by setting this field as true, the actual bitmap pixels are not
+		// loaded in the memory. Just the bounds are loaded. If
+		// you try the use the bitmap here, you will get null.
+		options.inJustDecodeBounds = true;
+		Bitmap bmp = BitmapFactory.decodeFile(filePath, options);
+
+		int actualHeight = options.outHeight;
+		int actualWidth = options.outWidth;
+
+		// max Height and width values of the compressed image is taken as
+		// 816x612
+
+		float maxHeight = 816.0f;
+		float maxWidth = 612.0f;
+		float imgRatio = actualWidth / actualHeight;
+		float maxRatio = maxWidth / maxHeight;
+
+		// width and height values are set maintaining the aspect ratio of the
+		// image
+
+		if (actualHeight > maxHeight || actualWidth > maxWidth) {
+			if (imgRatio < maxRatio) {
+				imgRatio = maxHeight / actualHeight;
+				actualWidth = (int) (imgRatio * actualWidth);
+				actualHeight = (int) maxHeight;
+			} else if (imgRatio > maxRatio) {
+				imgRatio = maxWidth / actualWidth;
+				actualHeight = (int) (imgRatio * actualHeight);
+				actualWidth = (int) maxWidth;
+			} else {
+				actualHeight = (int) maxHeight;
+				actualWidth = (int) maxWidth;
+
+			}
+		}
+
+		// setting inSampleSize value allows to load a scaled down version of
+		// the original image
+
+		options.inSampleSize = calculateInSampleSize(options, actualWidth,
+				actualHeight);
+
+		// inJustDecodeBounds set to false to load the actual bitmap
+		options.inJustDecodeBounds = false;
+
+		// this options allow android to claim the bitmap memory if it runs low
+		// on memory
+		options.inPurgeable = true;
+		options.inInputShareable = true;
+		options.inTempStorage = new byte[16 * 1024];
+
+		try {
+			// load the bitmap from its path
+			bmp = BitmapFactory.decodeFile(filePath, options);
+		} catch (OutOfMemoryError exception) {
+			exception.printStackTrace();
+
+		}
+		try {
+			scaledBitmap = Bitmap.createBitmap(actualWidth, actualHeight,
+					Bitmap.Config.ARGB_8888);
+		} catch (OutOfMemoryError exception) {
+			exception.printStackTrace();
+		}
+
+		float ratioX = actualWidth / (float) options.outWidth;
+		float ratioY = actualHeight / (float) options.outHeight;
+		float middleX = actualWidth / 2.0f;
+		float middleY = actualHeight / 2.0f;
+
+		Matrix scaleMatrix = new Matrix();
+		scaleMatrix.setScale(ratioX, ratioY, middleX, middleY);
+
+		Canvas canvas = new Canvas(scaledBitmap);
+		canvas.setMatrix(scaleMatrix);
+		canvas.drawBitmap(bmp, middleX - bmp.getWidth() / 2,
+				middleY - bmp.getHeight() / 2, new Paint(
+						Paint.FILTER_BITMAP_FLAG));
+
+		// check the rotation of the image and display it properly
+		ExifInterface exif;
+		try {
+			exif = new ExifInterface(filePath);
+
+			int orientation = exif.getAttributeInt(
+					ExifInterface.TAG_ORIENTATION, 0);
+			Log.d("EXIF", "Exif: " + orientation);
+			Matrix matrix = new Matrix();
+			if (orientation == 6) {
+				matrix.postRotate(90);
+				Log.d("EXIF", "Exif: " + orientation);
+			} else if (orientation == 3) {
+				matrix.postRotate(180);
+				//Log.d("EXIF", "Exif: " + orientation);
+			} else if (orientation == 8) {
+				matrix.postRotate(270);
+				//Log.d("EXIF", "Exif: " + orientation);
+			}
+			scaledBitmap = Bitmap.createBitmap(scaledBitmap, 0, 0,
+					scaledBitmap.getWidth(), scaledBitmap.getHeight(), matrix,
+					true);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		FileOutputStream out = null;
+		String filename = imageUri;
+		try {
+			out = new FileOutputStream(filename);
+
+			// write the compressed bitmap at the destination specified by
+			// filename.
+			scaledBitmap.compress(Bitmap.CompressFormat.JPEG, 30, out);
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		return filename;
+
+	}
+
+	public String getFilename() {
+		File file = new File(Environment.getExternalStorageDirectory()
+				.getPath(), "MyFolder/Images");
+		if (!file.exists()) {
+			file.mkdirs();
+		}
+		String uriSting = (file.getAbsolutePath() + "/"
+				+ System.currentTimeMillis() + ".jpg");
+		return uriSting;
+
+	}
+
+	private String getRealPathFromURI(String contentURI) {
+		Uri contentUri = Uri.parse(contentURI);
+		Cursor cursor = getContentResolver().query(contentUri, null, null,
+				null, null);
+		if (cursor == null) {
+			return contentUri.getPath();
+		} else {
+			cursor.moveToFirst();
+			int index = cursor
+					.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+			return cursor.getString(index);
+		}
+	}
+
+	public int calculateInSampleSize(BitmapFactory.Options options,
+			int reqWidth, int reqHeight) {
+		final int height = options.outHeight;
+		final int width = options.outWidth;
+		int inSampleSize = 1;
+
+		if (height > reqHeight || width > reqWidth) {
+			final int heightRatio = Math.round((float) height
+					/ (float) reqHeight);
+			final int widthRatio = Math.round((float) width / (float) reqWidth);
+			inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+		}
+		final float totalPixels = width * height;
+		final float totalReqPixelsCap = reqWidth * reqHeight * 2;
+		while (totalPixels / (inSampleSize * inSampleSize) > totalReqPixelsCap) {
+			inSampleSize++;
+		}
+
+		return inSampleSize;
+	}
+
+}
