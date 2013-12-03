e3c8f75b38720f14c1a947162256ef438d02e0ce
SirLolloDoD@gmail.com <SirLolloDoD@gmail.com@1bb21479-9aac-b773-212e-4918d035b54c>
2013-12-03 14:25:46 +0000

    Fix download user images. Lista conversazioni: mostrato numero nuovi messaggi. Chat singola: sistemata la visualizzazione dei nuovi messaggi ancora da leggere, anche se nuovi messaggi inseriti nel db.

--- trunk/UMessage/src/com/lollotek/umessage/adapters/PreviewChatAdapter.java
+++ trunk/UMessage/src/com/lollotek/umessage/adapters/PreviewChatAdapter.java
@@ -6,6 +6,7 @@ import java.util.Calendar;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,7 +26,9 @@ public class PreviewChatAdapter extends SimpleCursorAdapter {
 
 	private File mainFolder;
 	private String contactProfileImagesFolder;
-	
+
+	private Bundle newMessagesCount;
+
 	public PreviewChatAdapter(Context context, int layout, Cursor c,
 			String[] from, int[] to, int flags) {
 		super(context, layout, c, from, to, flags);
@@ -34,6 +37,10 @@ public class PreviewChatAdapter extends SimpleCursorAdapter {
 		this.cursor = c;
 		this.mainFolder = Utility.getMainFolder(context);
 		this.contactProfileImagesFolder = Settings.CONTACT_PROFILE_IMAGES_FOLDER;
+	}
+
+	public void setNewMessagesCount(Bundle b) {
+		this.newMessagesCount = b;
 	}
 
 	@Override
@@ -46,7 +53,7 @@ public class PreviewChatAdapter extends SimpleCursorAdapter {
 		newRow = inflater.inflate(R.layout.chatpreview, parent, false);
 		TextView name, message, time, news;
 		ImageView icon;
-		
+
 		cursor.moveToPosition(position);
 
 		name = (TextView) newRow.findViewById(R.id.textView1);
@@ -59,19 +66,25 @@ public class PreviewChatAdapter extends SimpleCursorAdapter {
 				.getColumnIndex(DatabaseHelper.KEY_MESSAGE));
 		String dataValue = cursor.getString(cursor
 				.getColumnIndex(DatabaseHelper.KEY_DATA));
-		boolean isSomeNewMessage = (cursor.getString(cursor
-				.getColumnIndex(DatabaseHelper.KEY_TOREAD))).equals("1");
+		// boolean isSomeNewMessage = (cursor.getString(cursor
+		// .getColumnIndex(DatabaseHelper.KEY_TOREAD))).equals("1");
 
-		if (isSomeNewMessage) {
+		int newMessages = newMessagesCount.getInt(cursor.getString(cursor
+				.getColumnIndex(DatabaseHelper.KEY_IDCHAT)), 0);
+		if (newMessages > 0) {
 			news = (TextView) newRow.findViewById(R.id.textView4);
+			news.setText("" + newMessages);
 			news.setVisibility(View.VISIBLE);
+
 		}
 
-		String iconSrc = cursor.getString(cursor.getColumnIndex(DatabaseHelper.KEY_IMGSRC));
-		
-		if(!iconSrc.equals("") && !iconSrc.equals("0")){
+		String iconSrc = cursor.getString(cursor
+				.getColumnIndex(DatabaseHelper.KEY_IMGSRC));
+
+		if (!iconSrc.equals("") && !iconSrc.equals("0")) {
 			icon = (ImageView) newRow.findViewById(R.id.imageView1);
-			File iconFile  = new File(mainFolder.toString() + contactProfileImagesFolder + iconSrc);
+			File iconFile = new File(mainFolder.toString()
+					+ contactProfileImagesFolder + iconSrc);
 			icon.setImageURI(Uri.fromFile(iconFile));
 		}
 		message.setText(messageValue);
